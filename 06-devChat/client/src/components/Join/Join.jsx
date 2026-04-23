a3c3c6a60fa2669beeb8a1680c7dbecc54e223f5
MaykThomazete <mayk.thomazete@aluno.senai.br>
2026-04-23 15:08:59 -0300

    feat: implementação do componente de chat e atualização do componente Join para gerenciamento de estado

--- 06-devChat/client/src/components/Join/Join.jsx
+++ 06-devChat/client/src/components/Join/Join.jsx
@@ -1,11 +1,50 @@
-import style from './Join.module.css'
+import { useRef } from "react";
+import style from "./Join.module.css";
 
-const Join = () => {
+import { Input, Button } from "@mui/material";
+import io from "socket.io-client";
+
+const Join = (props) => {
+  const usernameRef = useRef();
+
+  const handleSubmit = async () => {
+    const username = usernameRef.current.value;
+    if (!username.trim()) return;
+
+    const socket = io.connect("http://localhost:3001");
+    socket.emit("set_username", username);
+
+    props.setChatVisibility(true);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSubmit();
+    }
+  };
   return (
-    <div>
-      Join
-    </div>
-  )
-}
+    <>
+      <div className={style.dev_logo}>
+        <img src="" alt="Logo do DevChat" />
+      </div>
 
-export default Join
+      <div className={style.join_container}>
+        <h2>Bem-vindo ao devChat!</h2>
+        <Input
+          inputRef={usernameRef}
+          placeholder="Nome de usuário"
+          onKeyDown={handleKeyPress}
+        />
+        <Button
+          sx={{ mt: 2, mb: 2 }}
+          variant="contained"
+          onClick={() => handleSubmit()}
+        >
+          Entrar
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Join;
